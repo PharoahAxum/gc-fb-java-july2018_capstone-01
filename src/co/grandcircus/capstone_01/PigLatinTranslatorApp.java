2e0e1b41921a26a419e10436d5e751227bdb4e2d
louis-m-privette-iii <louis.privette@gmail.com>
2018-08-05 11:24:21 -0400

    First method made. Added some more pre-codeing planning notes.

--- src/co/grandcircus/capstone_01/PigLatinTranslatorApp.java
+++ src/co/grandcircus/capstone_01/PigLatinTranslatorApp.java
@@ -18,20 +18,43 @@ public class PigLatinTranslatorApp {
 		do {
 			System.out.println("\tEnter a line to be translated: ");
 			userInput = scnr.nextLine();
+			// format the string to ignore uppercase. (and later do one for special characters) 
+			
+			// go to methods:
+				// go to method for words that start with a vowel.
+				// go to method for words that start with a consonant.
 			
 			// test print
 			System.out.println(userInput);
 			
-			System.out.println("Do you want to translate another line? (y/n)");
+			// TODO add better validation. 
+			System.out.println("\tDo you want to translate another line? (y/n)");
 			yeaNay = scnr.nextLine();
 		} while (yeaNay.equals("y"));
 		
-		System.out.println("Goodbye.");
+			System.out.println("\tGoodbye.");
 		
 
 		scnr.close();
 
+		} 
+	
+	
+	// Method for words that start with a vowel.
+	public static String wordStartsWithVowel(String vowelFirst) {
+		// Simple method that adds 'way' at the end of the original word.
+		return vowelFirst + "way";
 	}
+
+	// Method for words that start with a consonant, hint via TA.)
+	//public static int findFirstInt (String word) {
+		//assume it takes in a word that starts with a consonant.
+	//}
+	
+	// Method for x, hint via TA.))
+	// public static String pigLatin (String word, ) {
+		//substring splits a word at index int index, flips it around and does the Pig Latin translation.
+	//}
 
 }
 
